53f4b1e2fb4fd23ba9f91f6db7eec20ee5cb2b4a
Roshan Razz <67270910+roshanrazz@users.noreply.github.com>
2022-06-12 17:27:47 +0530

    "updated branch"

--- src/components/Tipoff.js
+++ src/components/Tipoff.js
@@ -7,6 +7,7 @@ import styles from "../styles/Tipoff.module.css"
 
 // import "./App.css";
 // import { Axios } from "axios";
+// import { Axios } from "axios"
 
 class Tipoff extends React.Component {
   constructor(props) {
